ba4c82ba320f1cf19e780e39d344c3d12608883e
Keegan George <kgeorge13@gmail.com>
2021-10-13 14:14:20 -0700

    UX: Change preferences title to profile in user nav

--- javascripts/discourse/api-initializers/stemaway-theme.js
+++ javascripts/discourse/api-initializers/stemaway-theme.js
@@ -1,3 +1,11 @@
 import { apiInitializer } from 'discourse/lib/api';
 
-export default apiInitializer('0.11.1', (api) => {});
+export default apiInitializer('0.11.1', (api) => {
+  api.reopenWidget('user-menu-links', {
+    profileGlyph() {
+      const glyph = this._super(...arguments);
+      glyph['title'] = 'user.profile';
+      return glyph;
+    },
+  });
+});

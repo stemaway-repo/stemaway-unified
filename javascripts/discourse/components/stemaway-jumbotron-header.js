2c332776423b46fe4d400c81d49eb4db05ee36d3
jumagura <juangura@gmail.com>
2022-08-13 15:47:20 -0400

    Update stemaway-jumbotron-header.js

--- javascripts/discourse/components/stemaway-jumbotron-header.js
+++ javascripts/discourse/components/stemaway-jumbotron-header.js
@@ -1,4 +1,4 @@
-import GlimmerComponent from "discourse/components/glimmer";
+import GlimmerComponent from "@glimmer/component";
 import { ajax } from "discourse/lib/ajax";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 import I18n from "I18n";

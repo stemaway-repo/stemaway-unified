e558fc6fba29b637529892d6539b7d17d8ca5eb4
JB <joybugalia@gmail.com>
2026-02-23 00:20:30 -0500

    Update theme

--- javascripts/discourse/components/stemaway-footer-banner.js
+++ javascripts/discourse/components/stemaway-footer-banner.js
@@ -1,18 +1,24 @@
-import Component from "@glimmer/component";
-import { service } from "@ember/service";
-import { defaultHomepage } from "discourse/lib/utilities";
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import discourseComputed from 'discourse-common/utils/decorators';
+import { defaultHomepage } from 'discourse/lib/utilities';
 
-export default class StemawayFooterBanner extends Component {
-  @service router;
-  @service siteSettings;
+export default Component.extend({
+  router: service(),
+  tagName: '',
 
-  get canDisplay() {
-    const currentRouteName = this.router.currentRouteName;
+  @discourseComputed('router.currentRouteName')
+  canDisplay(currentRouteName) {
     if (currentRouteName === `discovery.${defaultHomepage()}`) {
       return true;
+    } else if (
+      this.siteSettings.top_menu
+        .split('|')
+        .any((m) => `discovery.${m}` === currentRouteName)
+    ) {
+      return true;
+    } else {
+      return false;
     }
-
-    const topMenu = (this.siteSettings.top_menu || "").split("|");
-    return topMenu.some((m) => `discovery.${m}` === currentRouteName);
-  }
-}
+  },
+});
--- javascripts/discourse/components/stemaway-jumbotron-header.js
+++ javascripts/discourse/components/stemaway-jumbotron-header.js
@@ -1,29 +1,27 @@
-import Component from "@glimmer/component";
-import { service } from "@ember/service";
+import GlimmerComponent from "@glimmer/component";
+import { ajax } from "discourse/lib/ajax";
+import { popupAjaxError } from "discourse/lib/ajax-error";
 import I18n from "I18n";
+import { tracked } from "@glimmer/tracking";
 
-
-export default class StemawayJumbotronHeader extends Component {
-  @service currentUser;
-
+export default class StemawayJumbotronHeader extends GlimmerComponent {
   get hero() {
-    const user = this.currentUser;
+    const currentUser = this.currentUser;
+    const groups = currentUser?.groups;
     const firstGroup = Number(settings.hero_group_1);
     const secondGroup = Number(settings.hero_group_2);
-
     let activeGroup = 3;
 
-    if (user && Array.isArray(user.groups)) {
-      for (const g of user.groups) {
-        if (g?.id === firstGroup) {
-          activeGroup = 1;
-          break;
-        }
-        if (g?.id === secondGroup) {
-          activeGroup = 2;
-          break;
-        }
-      }
+    if (currentUser) {
+      if (
+        groups.some((g) => {
+          if (g.id === firstGroup) {
+            activeGroup = 1;
+          } else if (g.id === secondGroup) {
+            activeGroup = 2;
+          }
+        })
+      );
     }
 
     return {
